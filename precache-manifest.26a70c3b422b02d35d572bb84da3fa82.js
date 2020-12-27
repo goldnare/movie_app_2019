6d8ead3832a9bee28f859bdb7bccedaf45fa627d
pmk <goldnare@naver.com>
2020-12-27 17:42:43 +0900

    Updates

---
+++ precache-manifest.26a70c3b422b02d35d572bb84da3fa82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d9f78f086b521ab3ffbda6a1742948",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "bb87643f985728d640b0",
+    "url": "/movie_app_2019/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/movie_app_2019/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "bb87643f985728d640b0",
+    "url": "/movie_app_2019/static/js/main.0ff102a4.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
